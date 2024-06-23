9f3473ecfc204f7f3ce236cbbf8ac67d5912e667
Tarik Bozüslü <boz.tarik38@gmail.com>
2024-06-23 20:56:12 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+async function fetchDataJson() {
+    let response = await fetch('db.json');
+    let responseAsJson = await response.json();
+    console.log(responseAsJson);
+}
+
+
+async function fetchDataText() {
+    let response = await fetch('h1.txt');
+    let responseAsJson = await response.json();
+    document.getElementById("content").innerHTML = responseAsText
+}
